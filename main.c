7a7f194f2bdc5c8582f56c856bc36a3c873b2a67
Lepromax <algowork@gmail.com>
2019-03-20 22:39:25 +0300

    Debug

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main () {
 
 	// Declaration and define array for a string
 	const int MAXLINE = 1000;
-	int line[MAXLINE];
+	char line[MAXLINE];
 	int len; // Length of the current string
 	for (int i = 0; i < MAXLINE; line[i++] = '\0');
 
