a3372ae47e7941b4a0296177936a2133bcb42692
Lepromax <algowork@gmail.com>
2019-03-21 21:31:56 +0300

    Debug

--- mylib.h
+++ mylib.h
@@ -581,7 +581,7 @@ void lower (char line[]) {
 void printChar () {
     
     for (int i = 32; i < 127; i++) {
-        printf ("%c%c", i, (i%10 == 7) ? '\n' : ' ');
+        printf ("%c%c", i, (i%10 == 5) ? '\n' : ' ');
     }
     printf ("\n");
 }
