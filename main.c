7c1169de43b83471515e6ebba8e1c4d7079fd7ae
Lepromax <algowork@gmail.com>
2019-03-22 16:12:36 +0300

    Debug

--- main.c
+++ main.c
@@ -5,11 +5,11 @@ int main () {
 	int arr[100];
 	int i;
 
-	for (i = 0; i < 100; i++) {
+	for (i = 1; i < 100; i++) {
 		arr[i] = i * i;
 
 	}
-	for (i = 0; i < 100; i++) {
+	for (i = 1; i < 100; i++) {
 		printf ("%4d - %4d%c", i, arr[i], (i%5) ? '|' : '\n');
 
 	}
