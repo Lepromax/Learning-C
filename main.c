d5dc24f226d73b5f87039f08fe83a7b40887b0bb
Lepromax <algowork@gmail.com>
2019-03-22 21:51:07 +0300

    just commit

--- main.c
+++ main.c
@@ -1,7 +1,25 @@
 #include "mylib.h"
 
 int main () {
-	
+	const int SIZE = 100;
+	int arr[SIZE];
+	int i, x, index;
+
+	x = 3844
+
+	for (i = 0; i < SIZE; i++) {
+		arr[i] = i * i;
+
+	}
+	for (i = 0; i < SIZE; i++) {
+		printf ("%3d - %4d %c", i, arr[i], ((i+1)%5) ? '|' : '\n');
+
+	}
+	printf ("\n");
+
+	index = binSearch (x, arr, SIZE);
+
+	printf ("%3d - %4d\n", index, x);
 
 	return 0;
 }
