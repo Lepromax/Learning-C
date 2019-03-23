1513d41b6571ce95fce185513ace8bc14a73db79
Lepromax <algowork@gmail.com>
2019-03-23 19:10:31 +0300

    Debug

--- main.c
+++ main.c
@@ -6,13 +6,9 @@ int main () {
 	char original[MAXLINE],
 		 changed[(int)1.5 * MAXLINE];
 	
-	for (int i = 0; i < MAXLINE; i++) {
-		original[i] = '\0';
-	}
-
 	getLine (original, MAXLINE); // Enter a string
 
-	escape (changed, original); // Copy original to change
+	//escape (changed, original); // Copy original to change
 
 	printf ("%s\n", changed);
 
